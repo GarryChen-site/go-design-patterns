fcb71f6fbc0c868dbe4a7c8a392d4df79420ee43
GarryChen <chenymj23@gmail.com>
2022-06-15 20:18:56 +0800

    delegation

---
+++ others/delegation.go
@@ -0,0 +1,69 @@
+package others
+
+import "fmt"
+
+type Widget struct {
+	X,Y int
+}
+
+type Label struct {
+	Widget
+	Text	string
+}
+
+type Button struct {
+	Label // Embedding (delegation)
+}
+    
+    
+type ListBox struct {
+	Widget          // Embedding (delegation)
+	Texts  []string // Aggregation
+	Index  int      // Aggregation
+}
+
+type Painter interface {
+	Paint()
+}
+
+type Clicker interface {
+	Click()
+}
+
+func (label Label) Paint() {
+	fmt.Printf("%p:Label.Paint(%q)\n", &label, label.Text)
+}
+      
+//因为这个接口可以通过 Label 的嵌入带到新的结构体，
+//所以，可以在 Button 中重载这个接口方法
+func (button Button) Paint() { // Override
+	  fmt.Printf("Button.Paint(%s)\n", button.Text)
+}
+
+func (button Button) Click() {
+	  fmt.Printf("Button.Click(%s)\n", button.Text)
+}
+      
+func (listBox ListBox) Paint() {
+	  fmt.Printf("ListBox.Paint(%q)\n", listBox.Texts)
+}
+      
+func (listBox ListBox) Click() {
+	  fmt.Printf("ListBox.Click(%q)\n", listBox.Texts)
+}
+
+func NewButton(x, y int, text string) *Button {
+	return &Button{Label{Widget{x, y}, text}}
+}
+
+func main() {
+	button1 := Button{Label{Widget{10, 70}, "OK"}}
+	button2 := NewButton(50,70, "Cancel")
+	listBox := ListBox{Widget{10, 40},
+		[]string{"AL","AK","AZ","AR"}, 0}
+
+	for _,painter := range []Painter{button1,button2,listBox} {
+		painter.Paint()
+	}
+
+}
